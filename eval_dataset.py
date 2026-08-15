0b8fb484959227d10f5f7c5d44ed3576dcdb87f6
Milka Gruber <gruber.milka@gmail.com>
2026-08-15 17:11:10 +0200

    inspection of the dataset MedMCQA that will be used as a evaluation dataset

---
+++ eval_dataset.py
@@ -0,0 +1,30 @@
+# load the evaluation dataset
+
+from datasets import load_dataset
+
+# load MedMCQA from Hugging Face
+dataset = load_dataset("openlifescienceai/medmcqa")
+
+# see dataset splits
+# we have 182 822 train, 4183 validation and 6150 test questions
+# 'id', 'question', 'opa', 'opb', 'opc', 'opd', 'cop', 'choice_type', 'exp', 'subject_name', 'topic_name'
+#print(dataset)
+
+# look at the first training example
+example = dataset["train"][0]
+
+print("\nFirst training example:")
+print(example)
+
+# Print it in a nicer format
+print("\nQuestion:")
+print(example["question"])
+
+print("\nOptions:")
+print("A:", example["opa"])
+print("B:", example["opb"])
+print("C:", example["opc"])
+print("D:", example["opd"])
+
+print("\nCorrect answer:")
+print(example["cop"])
